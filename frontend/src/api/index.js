8afddc62278c11c5c483c44508f68d58b8434c7c
André Orlandi <a.orlandi.o@gmail.com>
2021-08-06 11:47:13 -0300

    Initial commit

--- frontend/src/api/index.js
+++
@@ -1,8 +0,0 @@
-// Create WebSocket connection.
-const socket = new WebSocket('ws://localhost/websocket');
-
-// Connection opened
-socket.addEventListener('open', function (event) {
-  console.log('Connection oppened');
-  // socket.send(JSON.stringify({ state: { initialData: initialData } }))
-});
