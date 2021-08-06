6f18cd872cd94b3e7ec228b5620ed1f3b1b3e9e8
André Orlandi <a.orlandi.o@gmail.com>
2021-08-06 11:49:11 -0300

    Removed api folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react'
 import '@atlaskit/css-reset';
 import styled from 'styled-components'
 
-// API
-// import api from './api'
-
 const MessageWrapper = styled.div`
   width: 100vw;
   height: 100vh;
